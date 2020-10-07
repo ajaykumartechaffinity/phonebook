141bc8b36f3acf6953406998450141c7ed217e0a
Ajay Kumar R <ajaykumarr@tag188.tag.dc>
2020-10-07 17:19:19 +0530

    Added test case

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+from contacts.models import Contacts
+
+from django.test import TestCase
+
+
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
